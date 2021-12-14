30a5e6b8c5c42b755a4fdef04d7e0a83f69d3e70
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2021-12-14 11:26:07 +0800

    Update Base project

--- main.js
+++ main.js
@@ -12,8 +12,3 @@ AOS.init();
 
 // Set latest year on copyright notice
 $('#latestYear').text(new Date().getFullYear());
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
